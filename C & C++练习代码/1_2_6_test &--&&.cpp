52594689cf863280345b178e7ca39db2b06515bd
JamesonHuang <462430725@qq.com>
2014-09-19 13:04:32 +0800

    测试 & VS &&
    测试 & VS &&

---
+++ C & C++练习代码/1_2_6_test &--&&.cpp
@@ -0,0 +1,22 @@
+﻿/*********************************************************************
+	@ Title:		& VS &&
+
+	@ Description:		1、测试 & VS &&
+						
+	@ Conclusion:			
+
+	@ Author:		rh_Jameson
+
+	@ Date:			2014/09/19
+**********************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << (2 && 3) << endl;
+    cout << (2 & 3) << endl;
+    
+    return 0;
+}
